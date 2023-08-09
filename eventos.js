fa4998e754cbc2b0ac3df2b9889644a072774059
¨Brunovbf¨ <bvbf20@gmail.com>
2023-08-09 13:59:20 -0300

    commit 3

--- eventos.js
+++ eventos.js
@@ -1,6 +1,6 @@
 const divClick = document.getElementById("clickDiv");
-divClick.addEventListener('click', function(){
+divClick.addEventListener('click', function () {
 
     alert('Hola! Soy el div');
 
-})
+});
