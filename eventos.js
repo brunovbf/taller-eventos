38fd45e52bef046dbd58d1c5a8d3bbe7ef54a589
¨Brunovbf¨ <bvbf20@gmail.com>
2023-08-12 16:13:34 -0300

    Solucionclick

--- eventos.js
+++ eventos.js
@@ -1,4 +1,13 @@
+const button = document.querySelector('#clickDiv button');
 const divClick = document.getElementById("clickDiv");
+
+button.addEventListener('click', function(event){
+
+    event.stopPropagation();
+    alert('Hola!');
+
+})
+
 divClick.addEventListener('click', function () {
 
     alert('Hola! Soy el div');
